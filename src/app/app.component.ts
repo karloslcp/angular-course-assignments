295cee2939fd33278fc003037bb7079c22d6aa48
Karlos Chita <karloslcp@gmail.com>
2021-04-14 18:54:23 -0600

    Complete assignment!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	displayDetails = false;
+	numberOfClicks = [];
+	clickNumber = 0;
+
+	onDisplayDetails() {
+		this.clickNumber = this.clickNumber + 1;
+		this.numberOfClicks.push(this.clickNumber);
+		this.displayDetails = !this.displayDetails;
+	}
 }
